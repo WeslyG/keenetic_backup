3dc92c907e7ca7ec22eb6b0fa4e93ae5162d9bde
WeslyG <weslyg22@gmail.com>
2020-04-14 02:38:56 +0500

    external path + ansible-vault for config

--- magic.sh
+++ magic.sh
@@ -1,34 +1,48 @@
 #!/bin/sh
 
 # Change vars
-PASSWORD=admin
-USERNAME=admin
+PASSWORD='admin'
+USERNAME='admin'
 NUMBER_OF_BACKUPS=30
-ROUTER_HOST=192.168.1.1
+ROUTER_HOST='192.168.1.1'
 DATE=$(date +'%d-%m-%Y')
 FULLDATE=$(date +'%d-%m-%Y-%H:%M:%S')
 ELK_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-# Optional logging to elasticsearch (0 = disable)
+OUTPUT_PATH='/opt'
+
+#Optional logging to elasticsearch (0 = disable)
 ENABLE_LOGGING=0
 HOSTNAME_ELK=HOSTNAME:9200
+
+# Encrypt config with ansible-vault
+ANSIBLE_ENCRYPT=0
+ANSIBLE_PASSWORD='SuperSecureAnsiblePassword'
 
 #system var
 ERROR_CODE=1
 
 # DO BACKUP
-sshpass -p "${PASSWORD}" ssh "${USERNAME}@${ROUTER_HOST}" 'show running-config' > /opt/temp
+sshpass -p "${PASSWORD}" ssh "${USERNAME}@${ROUTER_HOST}" 'show running-config' > "${OUTPUT_PATH}/temp"
+
 if [ $? -eq 0 ]
 then
   ERROR_CODE=0
-  mv /opt/temp "/opt/keenetic-backup-${DATE}.txt"
+  if [ ${ANSIBLE_ENCRYPT} -eq 1 ]
+  then
+    # encrypt
+    echo "${ANSIBLE_PASSWORD}" > /tmp/dc53035d3b59492
+    ansible-vault encrypt "${OUTPUT_PATH}/temp" --vault-password-file /tmp/dc53035d3b59492 2> /dev/null
+    rm -rf /tmp/dc53035d3b59492
+  fi
+  mv "${OUTPUT_PATH}/temp" "${OUTPUT_PATH}/keenetic-backup-${DATE}.txt"
 fi
 
-count=$(ls -latc /opt/*.txt | wc -l)
+count=$(ls -latc ${OUTPUT_PATH}/*.txt | wc -l)
 # Check backup count
 if [ ${count} -gt ${NUMBER_OF_BACKUPS} ]
 then
-  file_to_del=$(find /opt/*.txt -type f -print0 | xargs -0 ls -ltr | head -n 1 | awk '{ print $9}')
+  file_to_del=$(find ${OUTPUT_PATH}/*.txt -type f -print0 | xargs -0 ls -ltr | head -n 1 | awk '{ print $9}')
   # remove oldest file
   rm -rf "${file_to_del}"
 fi
